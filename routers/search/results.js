794ea16c06353fa9730cf803f66f703cdfd7d08c
Realmy <43610767+ItsRealmy@users.noreply.github.com>
2023-11-16 10:05:07 +0100

    Starter for the results endpoint

--- routers/search/results.js
+++ routers/search/results.js
@@ -1,7 +1,19 @@
 import express from 'express';
+import eta from '../../lib/connections/eta.js';
+import getTextResults from '../../lib/results/text.js';
 
 const router = express.Router();
 
-
+router.get('/', async (req, res) => {
+  let response;
+  try {
+    response = await getTextResults(req.query.q, req.query.p);
+  } catch (err) {
+    // TODO: Handle errors
+  }
+  
+  const html = await eta.renderAsync('/pages/results.eta', { data: response });
+  res.send(html);
+});
 
 export default router;
